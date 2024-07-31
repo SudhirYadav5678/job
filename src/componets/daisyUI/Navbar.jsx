e240c29dd5b51ca2b599a3544c7b934b6099963f
SudhirYadavv <sudhirkyadav51@gmail.com>
2024-07-31 23:18:30 +0530

    try

--- src/componets/daisyUI/Navbar.jsx
+++ src/componets/daisyUI/Navbar.jsx
@@ -15,18 +15,7 @@ function Navbar() {
                 <button className="btn btn-outline btn-primary m-2"><Link to="/jobs">Jobs</Link></button>
                 <button className="btn btn-outline btn-secondary m-2"><Link to="/resumes">Resumes</Link></button>
                 <button className="btn btn-outline btn-accent m-2"><Link to="/about">About</Link></button>
-                {/* <button className="btn btn-outline btn-primary m-2"><Link to="/login">Login</Link></button> */}
-                <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>open modal</button>
-                <dialog id="my_modal_3" className="modal">
-                    <div className="modal-box">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
-                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-                        </form>
-                        <h3 className="font-bold text-lg">Hello!</h3>
-                        <p className="py-4">Press ESC key or click on ✕ button to close</p>
-                    </div>
-                </dialog>
+                <button className="btn btn-outline btn-primary m-2"><Link to="/login">Login</Link></button>
                 <button className="btn btn-outline btn-primary m-2"><Link to="/signup">SignUp</Link></button>
             </div>
             {/* serach and avatar */}
